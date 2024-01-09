d9127ff0e35784672273ea123e5ed76e65b22fb6
Kevin Nakamura <grinkers@grinkers.net>
2024-01-10 00:06:01 +0900

    Use ADC (internal temperature sensor) instead of an incrementing int.

--- hello_edn.c
+++ hello_edn.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "hardware/gpio.h"
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
 
 void some_edn(char* edn);
 
@@ -13,14 +14,29 @@ int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
+    adc_init();
+    // Make sure GPIO is high-impedance, no pullups etc
+    adc_gpio_init(26);
+    // Select ADC input 0 (GPIO26)
+    adc_select_input(4);
+    adc_set_temp_sensor_enabled(true);
+
     char buf[200];
-    int count = 0;
-    while (true) {
+    while (1) {
         sleep_ms(500);
         gpio_put(LED_PIN, 1);
-        sprintf(buf, "{:count %d :foo #{1 2 3 2 42} :bar 9042.123}", count);
+
+        // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
+        const float conversion_factor = 3.3f / (1 << 12);
+        uint16_t result = adc_read();
+        printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor);
+
+        float temperature = 27 - (((result * conversion_factor) - 0.706) / 0.001721);
+        printf("Internal Temperature: %.2f degrees Celsius\n", temperature);
+
+        sprintf(buf, "{:temp %.2f :foo #{1 2 3 2 42}}", temperature);
         some_edn(buf);
-        count++;
+
         sleep_ms(500);
         gpio_put(LED_PIN, 0);
     }
